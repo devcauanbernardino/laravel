46e809ec71bbe75627bd390cc8828160a1b6596f
devcauanbernardino <devcauanbernardino@gmail.com>
2026-02-20 20:13:00 -0300

    secao introducao rotas finalizado

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [PrincipalController::class, 'principal']);
+
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
+
+Route::get('/contato', [ContatoController::class, 'contato']);
+
+// Route::get('/sobre-nos', function () {
+//     return 'Sobre-nos';
+// });
+
+// Route::get('/contato', function () {
+//     return 'Contato';
+// });
+
+/* verbo http
+
+get
+post
+put
+patch
+delete
+options
+
+*/

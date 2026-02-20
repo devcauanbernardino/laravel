858afe13b9ae2753bf818477f15d0dd376aca2cd
devcauanbernardino <devcauanbernardino@gmail.com>
2026-02-20 20:53:26 -0300

    rotas

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,13 @@ Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
 
 Route::get('/contato', [ContatoController::class, 'contato']);
 
+//nome, categoria, assunto, mensagem
+
+Route::get('/contato/{nome}/{categoria}',function(string $nome, string $categoria) {
+    echo 'Estamos aqui ' . $nome . ' - ' . $categoria;
+});
+
+
 // Route::get('/sobre-nos', function () {
 //     return 'Sobre-nos';
 // });

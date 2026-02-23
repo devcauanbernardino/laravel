e73068cdd50c5dbea195dbc9cd22f58ac4c335f3
devcauanbernardino <devcauanbernardino@gmail.com>
2026-02-23 18:13:07 -0300

    aula de manha

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,14 @@ Route::get('/contato', [ContatoController::class, 'contato']);
 
 //nome, categoria, assunto, mensagem
 
-Route::get('/contato/{nome}/{categoria}',function(string $nome, string $categoria) {
-    echo 'Estamos aqui ' . $nome . ' - ' . $categoria;
-});
+Route::get('/contato/{nome}/{categoria_id}'
+,function(string $nome = 'Desconhecido', int $categoria_id = 1){
+    echo "Estamos aqui: $nome - $categoria_id";
+})->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
 
+// Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem?}',function(string $nome, string $categoria, string $assunto, string $mensagem = 'mensagem nao informada') {
+//     echo "Estamos aqui $nome - $categoria - $assunto - $mensagem";
+// });
 
 // Route::get('/sobre-nos', function () {
 //     return 'Sobre-nos';
